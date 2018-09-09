9f75183ae17e2d96297bdf0dca714d5581994918
John Carroll <john@kode8.com>
2018-09-09 20:50:46 +0100

    Save

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
     <router-view/>
   </div>
 </template>
+
 <script lang="ts">
 import Vue from 'vue';
 import Header from '@/components/Header.vue';
@@ -22,11 +23,23 @@ export default Vue.extend({
   },
 });
 </script>
+
 <style lang="scss">
+
+  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,700');
+
+  html,
   body {
-    font-family: -apple-system, system-ui, BlinkMacSystemFont,
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: "Roboto", -apple-system, system-ui, BlinkMacSystemFont,
     "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+    font-weight: 400;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    color: $white;
   }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,6 +15,16 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+header {
+  background-color: $color-primary;
+  margin: 0;
+  padding: 10px;
 
+  h1 {
+    @include roboto-light();
+    font-size: 22px;
+    margin: 0;
+  }
+}
 </style>
