777b32362eb7ba1219b27b19c4007145cc7b5382
John Carroll <john@kode8.com>
2018-09-10 21:11:31 +0100

    UPDATE: Copy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1><span>LinkedIn banners that suck?</span> Aint nobody got time for that!<br />
+    <h1><span>LinkedIn banners suck?</span> Aint nobody got time for that!<br />
     Instead, create engaging banners (in minutes) <span>like a boss.</span></h1>
     <h2>Choose from over 1000 stunning templates, or <a href="/new"><b>just design your own.</b></a></h2>
     <h2 style="opacity:.5">Great for recruitment drives, company announcements, or call to actions.</h2>
